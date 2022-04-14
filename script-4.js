3b52f0dc69867df9026f27e840fd19f152800347
Parahot <1@7456.ru>
2022-04-14 10:42:55 +0300

    20 урок

--- script-4.js
+++ script-4.js
@@ -100,3 +100,38 @@ function done() {
     console.log('Я прошел этот урок!');
 }
 lernJS('JavaScript', done);
+
+// 20 урок
+
+let options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() { // метод makeTest, созданный вручную
+        console.log('Test-2');
+    }
+};
+console.log(options.name);
+delete options.name; // удалить св-во в объекте
+console.log(options);
+console.log(options['colors']['border']); // доступ к св-ву объекта внтури другого объекта
+const {border, bg} = options.colors; // деструктуризация (разделение на более мелкие) объекта
+console.log(bg);
+options.makeTest(); // запуск метода makeTest, созданного вручную
+
+// перебрать через цикл все ключи и их значения
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
+// метод получения ключей и подсчет их количества
+console.log(Object.keys(options).length);
