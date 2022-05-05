949743d9d31f4a4553752f7bcc0a5f274f64e0e3
Parahot <1@7456.ru>
2022-05-05 12:38:25 +0300

    23д урок-2-3-еще

--- script-4.js
+++ script-4.js
@@ -244,3 +244,19 @@ for (let i = 0; i <= 11; i++) {
     zvezda += '*';
 }
 console.log(result);
+
+// или так:
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)
