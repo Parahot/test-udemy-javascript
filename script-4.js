43308cf680d64c9fba28a0be128b398e3341ea10
Parahot <1@7456.ru>
2022-06-29 13:07:07 +0300

    Упражнение по написанию кода 13

--- script-4.js
+++ script-4.js
@@ -726,3 +726,53 @@ let personalMovieDB2 = {
         });
     }
 };
+
+// Упражнение по написанию кода 13
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    // вычисление общей площади всех магазинов, которая вычисляется как длина магазина, умноженная на его ширину;
+    let ploschad = 0;
+    data.shops.forEach((shop) => {
+        ploschad += shop.width * shop.length;
+    });
+    //console.log(ploschad);
+
+    // вычисление общего объема торгового центра, так как цена отопления указана в кубических метрах;
+    let obem = ploschad * data.height;
+    //console.log(obem);
+
+    // определение того, хватает ли бюджета на оплату такого объема; 
+    let limit = data.budget / data.moneyPer1m3;
+    //console.log(limit);
+    
+    if (limit >= obem) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    }
+}
+isBudgetEnough(shoppingMallData);
